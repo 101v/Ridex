18fab2d24e664aadb4e00aeeb264a9385f6285a9
Vimal Patel <vimal@imorphr.com>
2021-06-19 16:44:41 +0100

    Added Authentication Endpoint

--- lib/ridex/guardian.ex
+++ lib/ridex/guardian.ex
@@ -1,12 +1,14 @@
 defmodule Ridex.Guardian do
-  use Gardian, otp_app: :ridex
+  use Guardian, otp_app: :ridex
 
   def subject_for_token(%{id: user_id}, _claim) do
     {:ok, user_id}
   end
 
   def resource_from_claims(%{"sub" => subject}) do
-    nil -> {:error, :not_found}
-    user -> {:ok, user}
+    case Ridex.Repo.get(Ridex.User, subject) do
+      nil -> {:error, :not_found}
+      user -> {:ok, user}
+    end
   end
 end
